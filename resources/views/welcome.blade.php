22aa3c642191154b379ef6d2390601fcdd037097
Julius <juliuscedricjomena@gmail.com>
2017-07-13 14:07:25 -0700

    Changes in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
             
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Forum
                 </div>
 
                 <div class="links">
